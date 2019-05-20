200f9fdb4dddaf525b9007a29e113350a565f091
bhaskardegala <bhaskardegala33@gmail.com>
2019-05-20 15:36:04 +0530

    updated

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,54 +1,54 @@
 variable "client_id" {
-  type    = "string"
+  type        = "string"
   description = "please enter your client id"
 }
 
 variable "client_secret" {
-  type    = "string"
+  type        = "string"
   description = "please enter your client secret"
 }
 
 variable "subscription_id" {
-  type    = "string"
+  type        = "string"
   description = "please enter your subscription id"
 }
 
 variable "tenant_id" {
-  type    = "string"
+  type        = "string"
   description = "please enter your tenant id"
 }
 
 variable "location" {
-  type    = "string"
+  type        = "string"
   description = "In which location you are desired to create your resource"
 }
 
 variable "username_web_vm" {
-  type    = "string"
+  type        = "string"
   description = "please enter your user name which is used as login creadential for web_vm"
 }
 
 variable "password_web_vm" {
-  type    = "string"
+  type        = "string"
   description = "please enter your password which is used as login creadential for web_vm"
 }
 
 variable "username_business_vm" {
-  type    = "string"
+  type        = "string"
   description = "please enter your user name which is used as login creadential for business_vm"
 }
 
 variable "password_business_vm" {
-  type    = "string"
+  type        = "string"
   description = "please enter your password which is used as login creadential for business_vm"
 }
 
 variable "username_db_vm" {
-  type    = "string"
+  type        = "string"
   description = "please enter your user name which is used as login creadential for db_vm"
 }
 
 variable "password_db_vm" {
-  type    = "string"
+  type        = "string"
   description = "please enter your password which is used as login creadential for db_vm"
 }
