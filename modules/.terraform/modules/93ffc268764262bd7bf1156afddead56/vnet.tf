de4ca778890c6ef839da9945c69a7cc2f171fee3
bhaskardegala <bhaskardegala33@gmail.com>
2019-05-23 10:20:23 +0530

    updated

--- modules/.terraform/modules/93ffc268764262bd7bf1156afddead56/vnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "trail" {
-  name                = "${var.first}-vnet1"
-  location            = "${azurerm_resource_group.trail.location}"
-  resource_group_name = "${azurerm_resource_group.trail.name}"
-  address_space       = ["10.0.0.0/16"]
-}
